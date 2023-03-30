72cb45738d7a7087036d470dd423cb48ba0886a3
Diego Ortiz Hurtado <73130246+museortizmedia@users.noreply.github.com>
2023-03-30 15:35:43 -0500

    subida mecanica variableable

---
+++ Assets/Scripts/Mechanicals/LanzadorDeVariables/blockvariable.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blockvariable : MonoBehaviour
+{
+    [SerializeField] string[] tipoDeVariable = new string[] { "String", "Int", "Real", "Boolean" };
+    public int tipoVariableEscogido = 0;
+    
+    //Este script se le agregará a un bloque que puede ser agarrado por el jugador.
+    private void Awake()
+    {
+        //validamos que el objeto tenga un tag variableable que le permitirá al detector saber qué tipo de tag es
+        if(gameObject.tag!= "variableable")
+        {
+            #if UNITY_EDITOR
+                Debug.LogError("Objeto variableable sin tag asignado, porfavor asigne un tag y vuelvalo a intentar", gameObject.transform);
+                UnityEditor.EditorApplication.isPlaying = false;
+            #elif UNITY_WEBPLAYER
+                     Application.OpenURL(webplayerQuitURL);
+            #else
+                     Application.Quit();
+            #endif
+        }
+    }
+
+    public bool EresTipo(string tipo)
+    {
+        return tipoDeVariable[tipoVariableEscogido] == tipo;
+    }
+}
---
+++ Assets/Scripts/Mechanicals/LanzadorDeVariables/variableable.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class variableable : MonoBehaviour
+{
+    public int tipoDeVariableAceptada=0;
+    public int capacidad=3;
+    int cantidadRecibida=0;
+    private void Start()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Se ha recibido una variable, hay"+cantidadRecibida);
+        cantidadRecibida++;
+    }
+    public bool Cantidad()
+    {
+        return cantidadRecibida >= capacidad;
+    }
+}
